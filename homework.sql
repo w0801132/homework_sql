753a5b26c19989a17ab2793803eb073598e5a791
Melissa Wood <38088765+w0801132@users.noreply.github.com>
2018-10-27 09:53:24 -0400

    no errors

--- homework.sql
+++ homework.sql
@@ -125,20 +125,11 @@ GROUP BY last_name DESC;
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
 -- As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
 -- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
--- SELECT title
--- 	FROM film
--- 	WHERE title
--- 	IN (
--- 		SELECT title
--- 		FROM film
--- 		WHERE title LIKE "Q%"
--- 			OR (
--- 				SELECT title
--- 				FROM film
--- 				WHERE title LIKE "K%"
--- 				)
--- 		)
--- GROUP BY language_id;
+SELECT title
+ 	FROM film
+ 	WHERE title LIKE "Q%"
+    OR title LIKE "K%"
+	AND language_id = 1;
 
 
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
